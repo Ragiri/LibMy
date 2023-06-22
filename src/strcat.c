91c51a6d3fb50570b0b698a1ce939d08e2288412
tsunuky <morvanadri242@gmail.com>
2023-06-22 14:56:48 +0200

    strcat

---
+++ src/strcat.c
@@ -0,0 +1,13 @@
+#include <libstring.h>
+#include <efassert.h>
+
+char *strcat(char *dest, const char *src) {
+    unsigned const char *_src = (unsigned const char)src;
+	unsigned const char *_dest = dest;
+    
+    efassert("dest is null",_dest, return (0));
+    efassert("src is null", _src, return (0));
+	for (; *dest; ++dest);
+	while (*dest++ = *_src++);
+	return(_dest);
+}
