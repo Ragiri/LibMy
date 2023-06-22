79bd0b0ff3e20cd93da0ced45f9f438f9afce83c
Maaown - Leonard Viktor Pooch <leonard.pooch@hotmail.fr>
2023-06-22 14:24:04 +0200

    include

--- include/libstring.h
+++ include/libstring.h
@@ -20,9 +20,16 @@ size_t		my_strlen(const char	*str);
 
 char		*my_string(const char	*str);
 char		*our_string(const char	*str);
-//char        *my_strstr(const char *haystack, const char *needle);
-char        *strrchr(const char *s, int c);
+
+char        *my_strrchr(const char *s, int c);
+char        *my_strchr (const char* _str, int ch);
 int         my_strcmp(const char *s1, const char *s2);
+int         my_strcasecmp(const char *s1, const char *s2);
+char        *my_strcpy (char* dest, const char* _src);
+char        *my_strncpy (char* dest, const char* _src, size_t maxlen);
+size_t      my_strnlen(const char* _str, size_t maxlen);
+//char        *my_strstr(const char *haystack, const char *needle);
+
 
 #endif	/*	OURSTRING_H		*/
 
