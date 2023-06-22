5847783ea0e76bba5b241d31cd0f75c947351456
Rena <angelinalaure22@gmail.com>
2023-06-22 12:31:54 +0200

    change branch

---
+++ src/strstr.c
@@ -0,0 +1,24 @@
+/*
+** Jason Brillante "Damdoshi" -------------------------------------------------
+** Pentacle Technologie 2008-2023 ---------------------------------------------
+** EFRITS SAS 2022-2023 -------------------------------------------------------
+** Hanged Bunny Studio 2014-2021 ----------------------------------------------
+** La Caverne aux Lapins Noirs ------------------------------------------------
+** ----------------------------------------------------------------------------
+** MY Lib rena-----------------------------------------------------------------
+** ----------------------------------------------------------------------------
+*/
+
+#include	"../include/libstring.h"
+#include    "../include/efassert.h"
+
+char *my_strstr(const char *haystack, const char *needle)
+{
+
+    efassert("first string is null", haystack, return (0));
+    efassert("second string is null", needle, return (0));
+    efassert("no occurrence", my_strlen(haystack) < my_strlen(needle), return (0));
+
+    for ()
+        for()
+}
