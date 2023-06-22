647529350d0acdca149779f4d4841ddecb4bcc08
Rena <angelinalaure22@gmail.com>
2023-06-22 12:33:05 +0200

    merge

--- src/strstr.c
+++ src/strstr.c
@@ -9,16 +9,16 @@
 ** ----------------------------------------------------------------------------
 */
 
-#include	"../include/libstring.h"
-#include    "../include/efassert.h"
-
-char *my_strstr(const char *haystack, const char *needle)
-{
-
-    efassert("first string is null", haystack, return (0));
-    efassert("second string is null", needle, return (0));
-    efassert("no occurrence", my_strlen(haystack) < my_strlen(needle), return (0));
-
-    for ()
-        for()
-}
+//#include	"../include/libstring.h"
+//#include    "../include/efassert.h"
+//
+//char *my_strstr(const char *haystack, const char *needle)
+//{
+//
+//    efassert("first string is null", haystack, return (0));
+//    efassert("second string is null", needle, return (0));
+//    efassert("no occurrence", my_strlen(haystack) < my_strlen(needle), return (0));
+//
+//    for ()
+//        for()
+//}
